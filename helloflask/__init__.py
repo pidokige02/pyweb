9ae4b9f9894009d9036453f8a2acfb3c19d5853c
pidokige02 <pidokige0204@gmail.com>
2023-03-04 16:09:05 +0900

    Global Object : g

--- helloflask/__init__.py
+++ helloflask/__init__.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, g
 
 app = Flask(__name__)
+app.debug = True     # use only debug!!
+
+@app.before_request  # request 를 처리하기 전에 실행하여 주세요
+def before_request():
+    print("before_request!!!")
+    g.str = "한글"
 
 @app.route("/")
 def helloworld():
-    return "Hello Flask World!"
-
+    return "Hello World!" + getattr(g, 'str', '111')  # '111' 은 default 값임
